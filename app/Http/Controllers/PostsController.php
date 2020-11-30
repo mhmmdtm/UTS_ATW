2606b7af4ff024c36f603cdc75de187658a08e7e
mhmmdtm <muhammadtami1206@gmail.com>
2020-11-30 07:49:15 +0700

    Management data
    membuat manajemen data tabel category dan artikel

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Posts;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Posts::paginate(10);
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $category = Category::all();
+        return view('admin.posts.create', compact('category'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate([
+            'judul' => 'required',
+            'category_id' => 'required',
+            'content' => 'required',
+        ]);
+        $posts = Posts::create([
+            'judul' => $request->judul,
+            'category_id' => $required->category_id,
+            'content' => $request->content,
+        ])
+        return redirect()->route('posts.index')->with('success', 'Data berhasil disimpan');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
